da86febbc6218b5a45eb56c0296585a889c2bf2b
MaicolCode <paucarjhon07@outlook.com>
2024-10-23 16:37:48 -0500

    Se modifica el archivo principal del servidor con las rutas especificadas

--- api_pm/app.js
+++ api_pm/app.js
@@ -1,11 +1,13 @@
 import express from 'express'
 
+const productRoutes = require('./Routes/Products')
+const saleRoutes = require('./Routes/Sales')
+
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.send('Hola bienvenido a la API para la gestion de productos!')
-})
+app.use('/products', productRoutes)
+app.use('/sales', saleRoutes)
 
 app.listen(port, () => {
   console.log(`Server listener in port http://localhost:${port}`)
