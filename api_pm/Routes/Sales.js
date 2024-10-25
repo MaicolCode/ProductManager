2fdc2e1f214d87fc1517509236203da2a8395182
MaicolCode <paucarjhon07@outlook.com>
2024-10-25 16:49:25 -0500

    Agregando nuevas rutas a la ruta de ventas

--- api_pm/Routes/Sales.js
+++ api_pm/Routes/Sales.js
@@ -6,4 +6,20 @@ salesRouter.get('/', (req, res) => {
   res.send('Hola bienvenido a la API para la gestion de ventas!')
 })
 
+salesRouter.get('/sales/:id', (req, res) => {
+  res.send('Se esta buscando una venta!')
+})
+
+salesRouter.post('/sales', (req, res) => {
+  res.send('Hola bienvenido a la API para la gestion de ventas!')
+})
+
+salesRouter.put('/sales/:id', (req, res) => {
+  res.send('Hola bienvenido a la API para la gestion de ventas!')
+})
+
+salesRouter.delete('/sales/:id', (req, res) => {
+  res.send('Hola bienvenido a la API para la gestion de ventas!')
+})
+
 export default salesRouter
