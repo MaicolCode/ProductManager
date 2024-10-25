cf4dac706481fce79ace5c0b3d5d3187810a9c75
MaicolCode <paucarjhon07@outlook.com>
2024-10-25 16:48:38 -0500

    Agregando nuevas rutas a la ruta productos

--- api_pm/Routes/Products.js
+++ api_pm/Routes/Products.js
@@ -6,4 +6,20 @@ productRouter.get('/', (req, res) => {
   res.send('Hola bienvenido a la API para la gestion de productos!')
 })
 
+productRouter.get('/product/:id', (req, res) => {
+  res.send('Se esta buscando un producto!')
+})
+
+productRouter.post('/product', (req, res) => {
+  res.send('Hola bienvenido a la API para la gestion de productos!')
+})
+
+productRouter.put('/product/:id', (req, res) => {
+  res.send('Hola bienvenido a la API para la gestion de productos!')
+})
+
+productRouter.delete('/product/:id', (req, res) => {
+  res.send('Hola bienvenido a la API para la gestion de productos!')
+})
+
 export default productRouter
