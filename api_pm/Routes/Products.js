ecbf01463f4f218483b1388b0de7f51312b6fdd4
MaicolCode <paucarjhon07@outlook.com>
2024-10-27 16:57:27 -0500

    Comprobacion con la base de datos para obtener los productos

--- api_pm/Routes/Products.js
+++ api_pm/Routes/Products.js
@@ -1,10 +1,23 @@
 import express from 'express'
+import mysql from 'mysql2/promise'
+
+// DB Config
+const config = {
+  host: 'localhost',
+  user: 'root',
+  password: 'MaicolCodea',
+  database: 'tech_db',
+  port: 3306
+}
+
+const connection = await mysql.createConnection(config)
 
 const productRouter = express.Router()
 
-productRouter.get('/', (req, res) => {
+productRouter.get('/', async (req, res) => {
+  const [products] = await connection.query('SELECT * FROM products')
   res.json({
-    message: 'Hola bienvenido a la API para la gestion de productos!'
+    products
   })
 })
 
