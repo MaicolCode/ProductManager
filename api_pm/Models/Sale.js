65101e96960f468ee21d3d736b4101024ed030d2
MaicolCode <paucarjhon07@outlook.com>
2024-10-29 17:04:59 -0500

    Creacion de metodos dentro del modelo de ventas para la obtencion de datos desde BD

--- api_pm/Models/Sale.js
+++ api_pm/Models/Sale.js
@@ -1,3 +1,60 @@
-export default class Sale {
+import { connection } from '../utils/connection.js'
 
+export default class Sale {
+  static async getAll () {
+    const [sales] = await connection.query('SELECT * FROM sales')
+    return sales
+  }
+
+  static async getById (id) {
+    const [sale] = await connection.query('SELECT * FROM sales WHERE id = ?', [
+      id
+    ])
+    return sale[0]
+  }
+
+  static async create (productID, quantity) {
+    try {
+      await connection.query(
+        'INSERT INTO sales (product_id, quantity_sold) VALUES (?,?)',
+        [productID, quantity]
+      )
+    } catch (error) {
+      return error
+    }
+    const [sales] = await connection.query('SELECT * FROM sales')
+    return sales[sales.length - 1]
+  }
+
+  static async update (id, data) {
+    try {
+      await connection.query('UPDATE sales SET ? WHERE id = ?', [data, id])
+    } catch (error) {
+      return error
+    }
+    const [sale] = await connection.query('SELECT * FROM sales WHERE id = ?', [
+      id
+    ])
+    if (sale.length === 0) {
+      return 'Venta no encontrada'
+    }
+    return 'Venta actualizada con exito'
+  }
+
+  static async delete (id) {
+    const [sale] = await connection.query('SELECT * FROM sales WHERE id = ?', [
+      id
+    ])
+
+    if (sale.length === 0) {
+      return 'Venta no encontrada'
+    }
+    try {
+      await connection.query('DELETE FROM sales WHERE id = ?', [id])
+    } catch (error) {
+      return error
+    }
+
+    return 'Venta eliminada con exito'
+  }
 }
