2fe2df1fd6114f3bb61636735791320e1bc9717f
MaicolCode <paucarjhon07@outlook.com>
2024-10-29 16:53:10 -0500

    Actualizacion de rutas con el controlador de las ventas

--- api_pm/Routes/Sales.js
+++ api_pm/Routes/Sales.js
@@ -1,76 +1,16 @@
 import express from 'express'
-import { connection } from '../utils/connection.js'
+import SalesController from '../Controllers/SalesController.js'
 
 const salesRouter = express.Router()
 
-salesRouter.get('/', async (req, res) => {
-  const [sales] = await connection.query('SELECT * FROM sales')
-  res.json({ sales })
-})
+salesRouter.get('/', SalesController.getAll)
 
-salesRouter.get('/:id', async (req, res) => {
-  const { id } = req.params
-  const [sale] = await connection.query('SELECT * FROM sales WHERE id = ?', [
-    id
-  ])
+salesRouter.get('/:id', SalesController.getById)
 
-  res.json({ sale })
-})
+salesRouter.post('/add', SalesController.create)
 
-salesRouter.post('/add', async (req, res) => {
-  const { productID, quantity } = req.body
+salesRouter.put('/:id', SalesController.update)
 
-  try {
-    await connection.query(
-      'INSERT INTO sales (product_id, quantity_sold) VALUES (?,?)',
-      [productID, quantity]
-    )
-  } catch (error) {
-    return res.status(500).json({ error: 'Error al agregar venta' })
-  }
-
-  const [sales] = await connection.query('SELECT * FROM sales')
-
-  res.json({ sale: sales[sales.length - 1] })
-})
-
-salesRouter.put('/:id', async (req, res) => {
-  const { id } = req.params
-  const data = req.body
-
-  try {
-    await connection.query('UPDATE sales SET ? WHERE id = ?', [data, id])
-  } catch (error) {
-    return res.status(500).json({ error: 'Error al actualizar venta' })
-  }
-
-  const [sale] = await connection.query('SELECT * FROM sales WHERE id = ?', [
-    id
-  ])
-
-  if (sale.length === 0) {
-    return res.status(404).json({ error: 'No se encontro la venta' })
-  }
-
-  res.json({ message: 'Venta actualizada con exito' })
-})
-
-salesRouter.delete('/:id', async (req, res) => {
-  const { id } = req.params
-  try {
-    await connection.query('DELETE FROM sales WHERE id = ?', [id])
-  } catch (error) {
-    return res.status(500).json({ error: 'Error al eliminar venta' })
-  }
-
-  const [sale] = await connection.query('SELECT * FROM sales WHERE id = ?', [
-    id
-  ])
-
-  if (sale.length === 0) {
-    return res.status(404).json({ error: 'No se encontro la venta' })
-  }
-  res.json({ message: 'Venta eliminada con exito' })
-})
+salesRouter.delete('/:id', SalesController.delete)
 
 export default salesRouter
