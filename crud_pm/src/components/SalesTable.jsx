0835187a9afdf131eb8cd78901c502c74e922b71
MaicolCode <paucarjhon07@outlook.com>
2024-10-30 17:17:55 -0500

    Modificacion del componente y fetching de datos a api para obtener datos del producto por id de venta

--- crud_pm/src/components/SalesTable.jsx
+++ crud_pm/src/components/SalesTable.jsx
@@ -1,15 +1,28 @@
+import { useEffect, useState } from 'react'
+
 function GetData(id) {
   const [data, setData] = useState(null)
+
   useEffect(() => {
-    fetch(`http://localhost:3000/products/${id}`)
-      .then((res) => res.json())
-      .then((data) => setData(data))
-  }, [])
+    const fetchData = async () => {
+      try {
+        const res = await fetch(`http://localhost:3000/products/${id}`)
+        const result = await res.json()
+        setData(result.product)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+
+    fetchData()
+  }, [id])
+
   return data
 }
 
 export default function SalesTable({ data }) {
   console.log(data)
+
   return (
     <table className='table-auto border-collapse border border-black'>
       <thead>
@@ -18,6 +31,7 @@ export default function SalesTable({ data }) {
           <th>Nombre</th>
           <th>Precio</th>
           <th>Cantidad</th>
+          <th>Total</th>
           <th>Fecha</th>
         </tr>
       </thead>
@@ -25,8 +39,14 @@ export default function SalesTable({ data }) {
         {data.map((item) => (
           <tr key={item.id}>
             <td>{item.id}</td>
-            <td>{item.id_product}</td>
+            <td>{GetData(item.product_id)?.name}</td>
+            <td>{GetData(item.product_id)?.price}</td>
             <td>{item.quantity_sold}</td>
+            <td>
+              {(item.quantity_sold * GetData(item.product_id)?.price).toFixed(
+                2
+              )}
+            </td>
             <td>{item.date_sale}</td>
           </tr>
         ))}
