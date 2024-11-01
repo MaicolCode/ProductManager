887c64f7edd18a0c7e343b12a89256f03358f5b1
MaicolCode <paucarjhon07@outlook.com>
2024-11-01 17:02:41 -0500

    Se realiza el cambio de diseño del boton creado

--- crud_pm/src/components/Button.jsx
+++ crud_pm/src/components/Button.jsx
@@ -1,6 +1,9 @@
 export default function Button({ action, title, color }) {
   return (
-    <button onClick={action} className={`bg-${color}-500`}>
+    <button
+      onClick={action}
+      className={`hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow bg-${color}-500`}
+    >
       {title}
     </button>
   )
