7aceae58bd072522684f1ceadb138348dcdd70bb
MaicolCode <paucarjhon07@outlook.com>
2024-11-02 13:38:04 -0500

    Modificacion de props y entrada del evento al hacer click

--- crud_pm/src/components/Button.jsx
+++ crud_pm/src/components/Button.jsx
@@ -1,7 +1,9 @@
-export default function Button({ action, title, color }) {
+import { useState } from 'react'
+
+export default function Button({ onDelete, identifier, title, color }) {
   return (
     <button
-      onClick={action}
+      onClick={() => onDelete(identifier)}
       className={`hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow bg-${color}-500`}
     >
       {title}
