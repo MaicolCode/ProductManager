28c6de1d9a8b51331c6264bfb7b7baa9d3b2e36b
MaicolCode <paucarjhon07@outlook.com>
2024-11-05 17:29:45 -0500

    Creacion del componente para actualizar ventas

---
+++ crud_pm/src/components/modals/ModalUpdateSale.jsx
@@ -0,0 +1,82 @@
+import { useState } from 'react'
+import useProduct from '../../hooks/useProduct'
+import CloseIcon from '../../icons/close'
+
+export default function ModalUpdateSale({ sale, updateSale }) {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const { products } = useProduct()
+
+  const handleOpen = () => setIsOpen(true)
+  const handleClose = () => setIsOpen(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const data = new FormData(e.target)
+    const newSale = {
+      productID: data.get('productID'),
+      quantity: data.get('quantity')
+    }
+
+    updateSale(sale.id, newSale)
+    handleClose()
+  }
+
+  return (
+    <>
+      <button
+        onClick={handleOpen}
+        className={`hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow bg-blue-500`}
+      >
+        Update
+      </button>
+      {isOpen && (
+        <div className='fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm flex justify-center items-center'>
+          <div className='w-[450px] relative bg-white px-5 pt-2 pb-5 rounded flex flex-col justify-center items-center gap-5 '>
+            <div className='w-full h-2'>
+              <button
+                className='text-slate-700 absolute right-5 rounded-sm p-1'
+                onClick={handleClose}
+              >
+                <CloseIcon />
+              </button>
+            </div>
+            <h1 className='w-full font-semibold text-center mb-3 text-slate-600'>
+              Actualizacion de ventas al sistema.
+            </h1>
+            <form
+              className='w-full flex flex-col gap-4 text-sm'
+              onSubmit={handleSubmit}
+            >
+              <label className='text-gray-700'>Producto:</label>
+              <input
+                type='text'
+                name='product'
+                defaultValue={sale.product_name}
+                readOnly
+                className='border border-slate-400 border-opacity-40 rounded-lg p-2'
+              />
+              <label className='text-gray-700'>Cantidad a vendida:</label>
+              <input
+                type='number'
+                step={1}
+                min={0}
+                name='quantity'
+                defaultValue={sale.quantity}
+                className='border border-slate-400 border-opacity-40 rounded-lg p-2'
+              />
+
+              <button
+                className='bg-blue-500 py-2 px-5 mt-5 rounded-lg text-white font-medium'
+                type='submit'
+              >
+                Guardar
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
+    </>
+  )
+}
