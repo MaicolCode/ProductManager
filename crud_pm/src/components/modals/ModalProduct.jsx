e2b98214dfb59c1a217966b1aff2230f2ba3692e
MaicolCode <paucarjhon07@outlook.com>
2024-11-05 15:45:53 -0500

    Evento de guardado para el formulario de productos

--- crud_pm/src/components/modals/ModalProduct.jsx
+++ crud_pm/src/components/modals/ModalProduct.jsx
@@ -1,11 +1,27 @@
 import { useState } from 'react'
 import CloseIcon from '../../icons/close'
+import useProduct from '../../hooks/useProduct'
 
 export default function ModalProduct() {
   const [isOpen, setIsOpen] = useState(false)
+  const { addProduct } = useProduct()
 
   const handleOpen = () => setIsOpen(true)
   const handleClose = () => setIsOpen(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const data = new FormData(e.target)
+    const newProduct = {
+      name: data.get('name'),
+      price: data.get('price'),
+      quantity: data.get('quantity')
+    }
+
+    addProduct(newProduct)
+    handleClose()
+  }
 
   return (
     <>
@@ -29,15 +45,20 @@ export default function ModalProduct() {
             <h1 className='w-full font-semibold text-center mb-3 text-slate-600'>
               Ingreso de Productos al sistema.
             </h1>
-            <form className='w-full flex flex-col gap-4 text-sm'>
+            <form
+              className='w-full flex flex-col gap-4 text-sm'
+              onSubmit={handleSubmit}
+            >
               <label className='text-gray-700'>Producto a ingresar:</label>
               <input
                 type='text'
+                name='name'
                 className='w-full border border-slate-400 border-opacity-40 rounded-lg px-2 py-2'
               />
               <label className='text-gray-700'>Valor del producto:</label>
               <input
                 type='text'
+                name='price'
                 className='w-full border border-slate-400 border-opacity-40 rounded-lg px-2 py-2'
               />
               <label className='text-gray-700'>Cantidad comprada:</label>
@@ -45,10 +66,14 @@ export default function ModalProduct() {
                 type='number'
                 step={1}
                 min={0}
+                name='quantity'
                 className='border border-slate-400 border-opacity-40 rounded-lg p-2'
               />
 
-              <button className='bg-blue-500 py-2 px-5 rounded-lg text-white font-medium'>
+              <button
+                className='bg-blue-500 py-2 px-5 rounded-lg text-white font-medium'
+                type='submit'
+              >
                 Guardar
               </button>
             </form>
