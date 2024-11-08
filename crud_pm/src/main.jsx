fd1f28480fd66a4f9b825802e5802efdaa1631fd
MaicolCode <paucarjhon07@outlook.com>
2024-11-08 15:57:38 -0500

    Agregando rutas pricipales a la aplicacion

--- crud_pm/src/main.jsx
+++ crud_pm/src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './styles.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>Pagina de inicio</h1>
+  },
+  {
+    path: '/dashboard',
+    element: <App />
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <>
-    <App />
+    <RouterProvider router={router} />
   </>
 )
