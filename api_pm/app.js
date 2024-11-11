4a260afddf440cc559d7f667bfec629e7d98867d
MaicolCode <paucarjhon07@outlook.com>
2024-11-10 22:29:13 -0500

    Se agregan los sufijos por los cuales van a estar enlazadas cada una de las nuevas rutas

--- api_pm/app.js
+++ api_pm/app.js
@@ -2,6 +2,9 @@ import express from 'express'
 import cors from 'cors'
 import productRouter from './Routes/Products.js'
 import salesRouter from './Routes/Sales.js'
+import loginRouter from './Routes/Login.js'
+import refreshTokenRouter from './Routes/RefreshToken.js'
+import usersRouter from './Routes/Users.js'
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -12,6 +15,9 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use('/products', productRouter)
 app.use('/sales', salesRouter)
+app.use('/login', loginRouter)
+app.use('/refreshToken', refreshTokenRouter)
+app.use('/users', usersRouter)
 
 app.listen(port, () => {
   console.log(`Server listener in port http://localhost:${port}`)
