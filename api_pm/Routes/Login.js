eb3a6074aed56a8b2ed72bef1609bea6ce74cae4
MaicolCode <paucarjhon07@outlook.com>
2024-11-11 23:42:52 -0500

    Creacion de endpoints para el logeo del usuario

--- api_pm/Routes/Login.js
+++ api_pm/Routes/Login.js
@@ -2,8 +2,22 @@ import { Router } from 'express'
 
 const loginRouter = Router()
 
-loginRouter.get('/', (req, resp) => {
-  resp.send('Login')
+loginRouter.post('/', (req, res) => {
+  const { username, password } = req.body
+
+  if (!username || !password) {
+    return res.status(400).json({ message: 'Missing parameters' })
+  }
+
+  const accessToken = 'AccessToken'
+  const refreshToken = 'RefreshToken'
+
+  const user = {
+    id: 1,
+    name: 'John Doe',
+    username: 'johndoe'
+  }
+  return res.status(200).json({ user, accessToken, refreshToken })
 })
 
 export default loginRouter
