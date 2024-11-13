f1b4b7888ac2b6dacddb41fd90f68ca62ad67617
MaicolCode <paucarjhon07@outlook.com>
2024-11-12 23:26:46 -0500

    Se agrega en el metodo de post para la validacion de datos del usuario y cracion de tokens para el acceso al sistema

--- api_pm/Routes/Login.js
+++ api_pm/Routes/Login.js
@@ -1,23 +1,68 @@
 import { Router } from 'express'
+import { connection } from '../utils/connection.js'
+import bcrypt from 'bcrypt'
+import {
+  generateAccessToken,
+  generateRefreshToken
+} from '../auth/generateToken.js'
+import { getUserData } from '../lib/getUserData.js'
 
 const loginRouter = Router()
 
-loginRouter.post('/', (req, res) => {
+loginRouter.post('/', async (req, res) => {
   const { username, password } = req.body
 
   if (!username || !password) {
     return res.status(400).json({ message: 'Missing parameters' })
   }
 
-  const accessToken = 'AccessToken'
-  const refreshToken = 'RefreshToken'
+  const [user] = await connection.query(
+    'SELECT BIN_TO_UUID(id) AS id, name, username, password FROM users WHERE username = ?',
+    [username]
+  )
 
-  const user = {
-    id: 1,
-    name: 'John Doe',
-    username: 'johndoe'
+  console.log(user[0])
+
+  if (user.length > 0) {
+    const isCorrect = await verifyPassword(password, user[0].password)
+
+    if (isCorrect) {
+      const accessToken = createAccessToken(user[0])
+      const refreshToken = await createRefreshToken(user[0])
+
+      return res
+        .status(200)
+        .json({ user: getUserData(user[0]), accessToken, refreshToken })
+    } else {
+      return res.status(400).json({ message: 'User or password incorrect' })
+    }
+  } else {
+    return res.status(400).json({ message: 'User not found' })
   }
-  return res.status(200).json({ user, accessToken, refreshToken })
 })
+
+async function verifyPassword(password, hash) {
+  return await bcrypt.compare(password, hash)
+}
+
+function createAccessToken(user) {
+  return generateAccessToken(getUserData(user))
+}
+
+// Cuidar la seguridad del refresh token
+
+async function createRefreshToken(user) {
+  const refreshToken = generateRefreshToken(getUserData(user))
+
+  try {
+    await connection.query(
+      'INSERT INTO refresh_tokens (id, refresh_token) VALUES (UUID_TO_BIN(UUID()), ?)',
+      [refreshToken]
+    )
+    return refreshToken
+  } catch (error) {
+    console.error(error)
+  }
+}
 
 export default loginRouter
