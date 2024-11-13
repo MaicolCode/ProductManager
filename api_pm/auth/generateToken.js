62bd5806d17c3c796eaf33eb4258d7ab63a39e02
MaicolCode <paucarjhon07@outlook.com>
2024-11-12 23:25:23 -0500

    Fichero para el almacenamiento de meotod s de autenticacion

---
+++ api_pm/auth/generateToken.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken'
+
+function sign(payload, isAccessToken) {
+  return jwt.sign(
+    payload,
+    isAccessToken
+      ? process.env.ACCESS_TOKEN_SECRET
+      : process.env.REFRESH_TOKEN_SECRET,
+    {
+      algorithm: 'HS256',
+      expiresIn: '10m'
+    }
+  )
+}
+
+export function generateAccessToken(user) {
+  return sign({ user }, true)
+}
+
+export function generateRefreshToken(user) {
+  return sign({ user }, false)
+}
