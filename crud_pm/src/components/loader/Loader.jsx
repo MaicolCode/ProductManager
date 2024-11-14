34199341af1206465d768ce10687a7e199bcfe1d
MaicolCode <paucarjhon07@outlook.com>
2024-11-14 18:41:44 -0500

    Creacion de fichero para componente de carga

---
+++ crud_pm/src/components/loader/Loader.jsx
@@ -0,0 +1,10 @@
+import Loading from '../../icons/Loading'
+
+export default function Loader() {
+  return (
+    <div className='w-full h-full flex flex-col justify-center items-center gap-5 bg-zinc-300 bg-opacity-10'>
+      <Loading />
+      Por favor espere mientras se carga el contenido...
+    </div>
+  )
+}
