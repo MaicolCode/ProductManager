d87dfd3ce4f91965f005d57cb2376f8bb1c80e73
MaicolCode <paucarjhon07@outlook.com>
2024-11-13 22:18:24 -0500

    Validacion de token generado

---
+++ api_pm/auth/validateToken.js
@@ -0,0 +1,11 @@
+import jwt from 'jsonwebtoken'
+
+function verifyAccessToken(token) {
+  return jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+}
+
+function verifyRefreshToken(token) {
+  return jwt.verify(token, process.env.REFRESH_TOKEN_SECRET)
+}
+
+export { verifyAccessToken, verifyRefreshToken }
