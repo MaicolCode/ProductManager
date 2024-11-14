76227a290e1672e04890b11682ac7d233257eb5d
MaicolCode <paucarjhon07@outlook.com>
2024-11-14 18:49:44 -0500

    Obtencion de todos los metodos provenientes del Provider para la carga de la pagina principal si esta autenticado

--- crud_pm/src/routes/ProtectedRoute.jsx
+++ crud_pm/src/routes/ProtectedRoute.jsx
@@ -4,5 +4,5 @@ import { useAuthUser } from '../auth/AuthProvider'
 export default function ProtectedRoute() {
   const userAuth = useAuthUser()
 
-  return <>{userAuth ? <Outlet /> : <Navigate to='/' />}</>
+  return <>{userAuth.isAuthenticated ? <Outlet /> : <Navigate to='/' />}</>
 }
