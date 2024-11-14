8e802efb86589c9112b5322eaeef6e1df96d4c74
MaicolCode <paucarjhon07@outlook.com>
2024-11-14 18:42:25 -0500

    Creacion de componente para el icono de carga a usar el componente de carga

---
+++ crud_pm/src/icons/Loading.jsx
@@ -0,0 +1,19 @@
+export default function Loading() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 24 24'
+      fill='none'
+      className='animate-spin'
+      stroke='currentColor'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      width={50}
+      height={50}
+      strokeWidth={2}
+    >
+      {' '}
+      <path d='M12 3a9 9 0 1 0 9 9'></path>{' '}
+    </svg>
+  )
+}
