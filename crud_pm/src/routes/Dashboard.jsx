097d98f9eab7844988e9341ae915b82633aa1655
MaicolCode <paucarjhon07@outlook.com>
2024-11-14 18:47:42 -0500

    Se agrega dentro del dashboard metodo para salir de la sesion

--- crud_pm/src/routes/Dashboard.jsx
+++ crud_pm/src/routes/Dashboard.jsx
@@ -1,26 +1,42 @@
 import { useAuthUser } from '../auth/AuthProvider'
-import ErrorPage from '../pages/ErrorPage'
+import { API_URL } from '../auth/constants'
 import Products from '../pages/Products'
 import Sales from '../pages/Sales'
-import {
-  Routes,
-  Route,
-  Link,
-  useNavigate,
-  Outlet,
-  Navigate
-} from 'react-router-dom'
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
 
 export default function Dashboard() {
   const navigate = useNavigate()
+  const { getRefreshToken, signOut } = useAuthUser()
+
+  async function handleClick() {
+    try {
+      const response = await fetch(`${API_URL}/signout`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${getRefreshToken()}`
+        }
+      })
+
+      if (response.ok) {
+        signOut()
+      } else {
+        const error = await response.json()
+        console.log(error)
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
-    <>
-      <div className='border-2 border-black rounded-lg p-4 m-4'>
+    <div className='h-dvh w-full p-5'>
+      <div className='min-h-full border-2 border-black rounded-lg p-4'>
         <section className='flex justify-between items-center'>
           <Navigation />
           <button
             className='p-2 bg-blue-500 text-white rounded-lg'
-            onClick={() => navigate('/')}
+            onClick={handleClick}
           >
             Logout
           </button>
@@ -35,22 +51,16 @@ export default function Dashboard() {
           <Outlet />
         </section>
       </div>
-    </>
+    </div>
   )
 }
 function Navigation() {
-  const userAuth = useAuthUser()
-
   return (
     <>
       <nav className='flex gap-5 p-5 bg-slate-50 w-full'>
         <Link to='/dashboard/products'>Productos</Link>
         <Link to='/dashboard/sales'>Ventas</Link>
-        {userAuth.type === 'admin' ? (
-          <Link to='/dashboard/users'>Usuarios</Link>
-        ) : (
-          <Navigate to='/dashboard' />
-        )}
+        <Link to='/dashboard/users'>Usuarios</Link>
       </nav>
     </>
   )
