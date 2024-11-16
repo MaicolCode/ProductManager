359a3397eca813ca73fb8e9ac655f1bd44db9aaf
MaicolCode <paucarjhon07@outlook.com>
2024-11-15 21:53:03 -0500

    Modificacion del contenido en que se renderizan las tablas

--- crud_pm/src/pages/Products.jsx
+++ crud_pm/src/pages/Products.jsx
@@ -6,7 +6,11 @@ export default function Products() {
   const { products, deleteProduct, addProduct, updateProduct } = useProduct()
 
   return (
-    <div>
+    <div className='w-full p-4'>
+      <h2 className='text-2xl font-semibold text-slate-700 my-6'>
+        Listado de productos
+      </h2>
+      <hr className='border-slate-200 w-full mb-2' />
       {products ? (
         <ProductsTable
           products={products}
