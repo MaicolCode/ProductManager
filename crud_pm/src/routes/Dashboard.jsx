7101d85995887425ae88405595c750107de68c94
MaicolCode <paucarjhon07@outlook.com>
2024-11-15 21:54:20 -0500

    Se estila el lugar en donde se renderizan cada uno de los componentes que tiene que visualizar el usuario

--- crud_pm/src/routes/Dashboard.jsx
+++ crud_pm/src/routes/Dashboard.jsx
@@ -2,7 +2,14 @@ import { useAuthUser } from '../auth/AuthProvider'
 import { API_URL } from '../auth/constants'
 import Products from '../pages/Products'
 import Sales from '../pages/Sales'
-import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
+import {
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+  Outlet,
+  NavLink
+} from 'react-router-dom'
 
 export default function Dashboard() {
   const navigate = useNavigate()
@@ -30,37 +37,71 @@ export default function Dashboard() {
   }
 
   return (
-    <div className='h-dvh w-full p-5'>
-      <div className='min-h-full border-2 border-black rounded-lg p-4'>
-        <section className='flex justify-between items-center'>
-          <Navigation />
-          <button
-            className='p-2 bg-blue-500 text-white rounded-lg'
-            onClick={handleClick}
-          >
-            Logout
-          </button>
+    <div className='h-dvh w-full p-3'>
+      <div className='h-full shadow-[0_0_10px_1px_rgba(0,0,0,0.1)] rounded-lg p-4 flex gap-5'>
+        <section className='lg:w-[200px] sm:w-[360px] flex flex-col justify-between items-center'>
+          <Navigation action={handleClick} />
         </section>
-
         <Routes>
           <Route path='/products' element={<Products />} />
           <Route path='/sales' element={<Sales />} />
         </Routes>
-
-        <section>
-          <Outlet />
-        </section>
+        <Outlet />
       </div>
     </div>
   )
 }
-function Navigation() {
+function Navigation({ action }) {
   return (
     <>
-      <nav className='flex gap-5 p-5 bg-slate-50 w-full'>
-        <Link to='/dashboard/products'>Productos</Link>
-        <Link to='/dashboard/sales'>Ventas</Link>
-        <Link to='/dashboard/users'>Usuarios</Link>
+      <nav className='h-full w-[200px]] flex flex-col justify-between gap-5 py-3 px-2 bg-slate-100 rounded-tl-md rounded-bl-lg text-sm'>
+        <section className='flex flex-col gap-2'>
+          <article className='relative w-[200px] h-[90px] flex justify-center items-center'>
+            <img
+              src='../public/images/logo.png'
+              alt='logo'
+              className='w-16 h-16 rounded-full'
+            />
+          </article>
+
+          <NavLink
+            to={`/dashboard/products`}
+            className={({ isActive }) =>
+              isActive
+                ? 'p-3 bg-gray-300 text-slate-500 bg-opacity-40 rounded-md'
+                : 'p-3 bg-gray-200 text-slate-500 bg-opacity-40 rounded-md'
+            }
+          >
+            Productos
+          </NavLink>
+
+          <NavLink
+            to={`/dashboard/sales`}
+            className={({ isActive }) =>
+              isActive
+                ? 'p-3 bg-gray-300 text-slate-500 bg-opacity-40 rounded-md'
+                : 'p-3 bg-gray-200 text-slate-500 bg-opacity-40 rounded-md'
+            }
+          >
+            Ventas
+          </NavLink>
+          <NavLink
+            to={`/dashboard/users`}
+            className={({ isActive }) =>
+              isActive
+                ? 'p-3 bg-gray-300 text-slate-500 bg-opacity-40 rounded-md'
+                : 'p-3 bg-gray-200 text-slate-500 bg-opacity-40 rounded-md'
+            }
+          >
+            Ventas
+          </NavLink>
+        </section>
+        <button
+          className='p-3 bg-gray-200 rounded-lg text-slate-500'
+          onClick={action}
+        >
+          Cerrar sesion
+        </button>
       </nav>
     </>
   )
