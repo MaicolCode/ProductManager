a4ae0ba79fe54ae7c38021ea25a25f58f4202a76
MaicolCode <paucarjhon07@outlook.com>
2024-11-16 21:28:48 -0500

    Agregando componente de hamburguesa para el menu

--- crud_pm/src/routes/Dashboard.jsx
+++ crud_pm/src/routes/Dashboard.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { useAuthUser } from '../auth/AuthProvider'
 import { API_URL } from '../auth/constants'
 import Products from '../pages/Products'
@@ -10,6 +11,8 @@ import {
   Outlet,
   NavLink
 } from 'react-router-dom'
+import MenuIcon from '../icons/Menu'
+import CloseIcon from '../icons/close'
 
 export default function Dashboard() {
   const navigate = useNavigate()
@@ -41,7 +44,11 @@ export default function Dashboard() {
       <div className='h-full shadow-[0_0_10px_1px_rgba(0,0,0,0.1)] rounded-lg p-4 flex gap-5'>
         <section className='lg:w-[200px] sm:w-[360px] flex flex-col justify-between items-center'>
           <Navigation action={handleClick} />
+          <section className='relative h-full flex sm:hidden gap-2'>
+            <Hamburger action={handleClick} />
+          </section>
         </section>
+
         <Routes>
           <Route path='/products' element={<Products />} />
           <Route path='/sales' element={<Sales />} />
@@ -51,10 +58,11 @@ export default function Dashboard() {
     </div>
   )
 }
+
 function Navigation({ action }) {
   return (
     <>
-      <nav className='h-full w-[200px]] flex flex-col justify-between gap-5 py-3 px-2 bg-slate-100 rounded-tl-md rounded-bl-lg text-sm'>
+      <nav className='h-full w-[200px] hidden sm:flex flex-col justify-between  gap-5 py-3 px-2 bg-slate-100 rounded-tl-md rounded-bl-lg text-sm'>
         <section className='flex flex-col gap-2'>
           <article className='relative w-[200px] h-[90px] flex justify-center items-center'>
             <img
@@ -104,5 +112,75 @@ function Navigation({ action }) {
         </button>
       </nav>
     </>
+  )
+}
+
+function Hamburger({ action }) {
+  const [visible, setVisible] = useState(false)
+
+  const handleClick = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div className='absolute h-full flex flex-col gap-1 z-10 top-0'>
+      <button
+        className='h-10 w-10 p-3 sm:hidden bg-slate-100 rounded-lg text-slate-500 flex justify-center items-center'
+        onClick={handleClick}
+      >
+        {!visible ? <MenuIcon /> : <CloseIcon />}
+      </button>
+      {visible && (
+        <nav className='h-full w-[200px] flex sm:hidden flex-col justify-between  gap-5 py-3 px-2 bg-slate-100 rounded-md text-sm'>
+          <section className='flex flex-col gap-2'>
+            <article className='relative w-[200px] h-[90px] flex justify-center items-center'>
+              <img
+                src='../public/images/logo.png'
+                alt='logo'
+                className='w-16 h-16 rounded-full'
+              />
+            </article>
+
+            <NavLink
+              to={`/dashboard/products`}
+              className={({ isActive }) =>
+                isActive
+                  ? 'p-3 bg-gray-300 text-slate-500 bg-opacity-40 rounded-md'
+                  : 'p-3 bg-gray-200 text-slate-500 bg-opacity-40 rounded-md'
+              }
+            >
+              Productos
+            </NavLink>
+
+            <NavLink
+              to={`/dashboard/sales`}
+              className={({ isActive }) =>
+                isActive
+                  ? 'p-3 bg-gray-300 text-slate-500 bg-opacity-40 rounded-md'
+                  : 'p-3 bg-gray-200 text-slate-500 bg-opacity-40 rounded-md'
+              }
+            >
+              Ventas
+            </NavLink>
+            <NavLink
+              to={`/dashboard/users`}
+              className={({ isActive }) =>
+                isActive
+                  ? 'p-3 bg-gray-300 text-slate-500 bg-opacity-40 rounded-md'
+                  : 'p-3 bg-gray-200 text-slate-500 bg-opacity-40 rounded-md'
+              }
+            >
+              Ventas
+            </NavLink>
+          </section>
+          <button
+            className='p-3 bg-gray-200 rounded-lg text-slate-500'
+            onClick={action}
+          >
+            Cerrar sesion
+          </button>
+        </nav>
+      )}
+    </div>
   )
 }
