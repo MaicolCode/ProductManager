ca2d846e06ec3e07b88816501974bb204ddf3f9d
MaicolCode <paucarjhon07@outlook.com>
2024-11-17 08:03:43 -0500

    Agregando nuevo componente de icono para filtrar

---
+++ crud_pm/src/icons/Filter.jsx
@@ -0,0 +1,20 @@
+export default function FilterIcon() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      className='lucide lucide-list-filter'
+    >
+      <path d='M3 6h18' />
+      <path d='M7 12h10' />
+      <path d='M10 18h4' />
+    </svg>
+  )
+}
