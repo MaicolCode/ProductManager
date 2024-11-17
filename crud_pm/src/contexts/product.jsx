a6d46567a36da98bbd96a5bed5a11cdd9c0944ef
MaicolCode <paucarjhon07@outlook.com>
2024-11-16 23:41:36 -0500

    Contexto creado para el acceso a todos los metodos de productos y evitar props driling.

---
+++ crud_pm/src/contexts/product.jsx
@@ -0,0 +1,63 @@
+import { createContext, useEffect, useState } from 'react'
+
+export const ProductContext = createContext()
+
+export default function ProductProvider({ children }) {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    fetchProducts()
+  }, [])
+
+  const addProduct = async (product) => {
+    await fetch(`http://localhost:3000/products/add`, {
+      method: 'POST',
+      body: JSON.stringify(product),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    fetchProducts()
+  }
+
+  const updateProduct = async (id, product) => {
+    await fetch(`http://localhost:3000/products/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify(product),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    fetchProducts()
+  }
+
+  const deleteProduct = async (id) => {
+    await fetch(`http://localhost:3000/products/${id}`, {
+      method: 'DELETE'
+    })
+
+    setProducts(products.filter((product) => product.id !== id))
+  }
+
+  async function fetchProducts() {
+    const res = await fetch(`http://localhost:3000/products`)
+    const result = await res.json()
+    setProducts(result.products)
+  }
+
+  return (
+    <ProductContext.Provider
+      value={{
+        products,
+        addProduct,
+        updateProduct,
+        deleteProduct,
+        fetchProducts
+      }}
+    >
+      {children}
+    </ProductContext.Provider>
+  )
+}
