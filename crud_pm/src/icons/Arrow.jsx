ee02196627c5f96c206920b38b0ca0228aaebaac
MaicolCode <paucarjhon07@outlook.com>
2024-11-18 10:03:02 -0500

    Creacion de componente para el icono de Arrow

---
+++ crud_pm/src/icons/Arrow.jsx
@@ -0,0 +1,18 @@
+export default function ArrowIcon() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='15'
+      height='15'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      className='lucide lucide-chevron-right'
+    >
+      <path d='m9 18 6-6-6-6' />
+    </svg>
+  )
+}
