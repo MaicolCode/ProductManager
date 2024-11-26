2d2252a48ad4236d0c78daed7c6b94ba4a7e1f69
MaicolCode <paucarjhon07@outlook.com>
2024-11-25 21:42:09 -0500

    Creacion de clases para controlador y modelo para la obtencion de todos los usuarios

--- api_pm/Routes/Users.js
+++ api_pm/Routes/Users.js
@@ -1,9 +1,28 @@
 import { Router } from 'express'
+import { connection } from '../utils/connection.js'
 
 const usersRouter = Router()
+
+class User {
+  static async getAll() {
+    const [users] = await connection.query(
+      'SELECT name, username, permissions, type FROM users u, user_type ut WHERE u.permissions = ut.id;'
+    )
+    return users
+  }
+}
+
+class UsersController {
+  static async getAll(req, res) {
+    const result = await User.getAll()
+    res.status(200).json(result)
+  }
+}
 
 usersRouter.get('/', (req, resp) => {
   resp.status(200).json({ user: req.user })
 })
+
+usersRouter.get('/all', UsersController.getAll)
 
 export default usersRouter
