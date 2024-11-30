f1b2331f0f9a03171cf3a5350d9dd2a55edd6910
MaicolCode <paucarjhon07@outlook.com>
2024-11-29 19:33:24 -0500

    Se modifica el componente para que se adapte al contenido

--- crud_pm/src/components/loader/Loader.jsx
+++ crud_pm/src/components/loader/Loader.jsx
@@ -2,7 +2,7 @@ import Loading from '../../icons/Loading'
 
 export default function Loader() {
   return (
-    <div className='w-full h-screen flex flex-col justify-center items-center gap-5 bg-zinc-300 bg-opacity-10'>
+    <div className='w-full h-full flex flex-col justify-center items-center gap-5 bg-zinc-300 bg-opacity-10'>
       <Loading />
       Por favor espere mientras se carga el contenido...
     </div>
