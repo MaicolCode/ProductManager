c6f4fd86dd6eab7c96b688beefeb369be1792638
MaicolCode <paucarjhon07@outlook.com>
2024-12-04 20:14:19 -0500

    Se refactorizo la cantidad de codigo que tenia cada una de las rutas

--- api_pm/Routes/RefreshToken.js
+++ api_pm/Routes/RefreshToken.js
@@ -1,50 +1,8 @@
 import { Router } from 'express'
-import { getTokenFromHeader } from '../auth/getTokenFromHeader.js'
-import { connection } from '../utils/connection.js'
-import { verifyRefreshToken } from '../auth/validateToken.js'
-import { generateAccessToken } from '../auth/generateToken.js'
+import RefreshTokensController from '../Controllers/RefereshTokensController.js'
 
 const refreshTokenRouter = Router()
 
-refreshTokenRouter.post('/', async (req, resp) => {
-  const refreshToken = getTokenFromHeader(req.headers)
+refreshTokenRouter.post('/', RefreshTokensController.getRefreshToken)
 
-  if (refreshToken) {
-    try {
-      const found = await findOne(refreshToken)
-      if (!found) {
-        return resp.status(401).json({ message: 'Unauthorized not found ' })
-      }
-      const payload = verifyRefreshToken(found.refresh_token)
-
-      if (payload) {
-        const accessToken = generateAccessToken(payload.user)
-        resp.status(200).json({ accessToken })
-      } else {
-        return resp
-          .status(401)
-          .json({ message: 'Unauthorized not token access' })
-      }
-    } catch (error) {}
-  } else {
-    resp.status(401).json({ message: 'Unauthorized Link' })
-  }
-})
-
-async function findOne(refreshToken) {
-  try {
-    const [token] = await connection.query(
-      'SELECT * FROM refresh_tokens WHERE refresh_token = ?',
-      [refreshToken]
-    )
-
-    if (token.length > 0) {
-      return token[0]
-    } else {
-      return false
-    }
-  } catch (error) {
-    console.error(error)
-  }
-}
 export default refreshTokenRouter
--- api_pm/Routes/Users.js
+++ api_pm/Routes/Users.js
@@ -1,50 +1,8 @@
 import { Router } from 'express'
-import { connection } from '../utils/connection.js'
 import { authenticate } from '../auth/authenticate.js'
+import UsersController from '../Controllers/UsersController.js'
 
 const usersRouter = Router()
-
-class User {
-  static async getAll() {
-    const [users] = await connection.query(
-      'SELECT BIN_TO_UUID(u.id) id, name, username, permissions, ut.type type FROM users u, user_type ut WHERE u.permissions = ut.id'
-    )
-    return users
-  }
-
-  static async delete(id) {
-    try {
-      const result = await connection.query(
-        'DELETE FROM users WHERE id = UUID_TO_BIN(?)',
-        [id]
-      )
-
-      if (result.affectedRows > 0) {
-        return true
-      } else {
-        return false
-      }
-    } catch (error) {
-      throw new Error(error)
-    }
-  }
-}
-
-class UsersController {
-  static async getAll(req, res) {
-    const result = await User.getAll()
-    res.status(200).json(result)
-  }
-  static async delete(req, res) {
-    const { id } = req.params
-    const result = await User.delete(id)
-    if (result === true) {
-      res.status(200).json({ message: 'User deleted successfully' })
-    } else {
-      res.status(400).json({ message: 'User not found' })
-    }
-  }
-}
 
 usersRouter.get('/', authenticate, (req, resp) => {
   resp.status(200).json({ user: req.user })
