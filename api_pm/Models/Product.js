4c060aa5f80ee69fbe0b502582561e78b166ac35
MaicolCode <paucarjhon07@outlook.com>
2024-12-20 12:27:59 -0500

    Actualizacion en la obtencion de datos de los productos

--- api_pm/Models/Product.js
+++ api_pm/Models/Product.js
@@ -1,12 +1,14 @@
 import { connection } from '../utils/connection.js'
 
 export default class Product {
-  static async getAll () {
-    const [products] = await connection.query('SELECT * FROM products')
+  static async getAll() {
+    const [products] = await connection.query(
+      'SELECT p.id, p.name, p.price, p.quantity, c.name AS category FROM products p INNER JOIN categories c ON p.id_category = c.id ORDER BY c.name;'
+    )
     return products
   }
 
-  static async getById (id) {
+  static async getById(id) {
     const [product] = await connection.query(
       'SELECT * FROM products WHERE id = ?',
       [id]
@@ -14,7 +16,7 @@ export default class Product {
     return product[0]
   }
 
-  static async create (name, price, quantity) {
+  static async create(name, price, quantity) {
     try {
       await connection.query(
         'INSERT INTO products (name, price, quantity) VALUES (?, ?, ?)',
@@ -27,7 +29,7 @@ export default class Product {
     return product[product.length - 1]
   }
 
-  static async update (id, data) {
+  static async update(id, data) {
     try {
       await connection.query('UPDATE products SET ? WHERE id = ?', [data, id])
     } catch (error) {
@@ -43,14 +45,17 @@ export default class Product {
     return 'Producto actualizado con exito'
   }
 
-  static async delete (id) {
+  static async delete(id) {
     try {
       await connection.query('DELETE FROM products WHERE id = ?', [id])
     } catch (error) {
       return error
     }
 
-    const [product] = await connection.query('SELECT * FROM products WHERE id = ?', [id])
+    const [product] = await connection.query(
+      'SELECT * FROM products WHERE id = ?',
+      [id]
+    )
 
     if (!product) {
       return 'Producto no encontrado'
